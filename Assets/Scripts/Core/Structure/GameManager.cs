e3a075588f6f2260858e76259e591219b005f533
Andrii <101969951+MechanikXD@users.noreply.github.com>
2025-10-27 11:47:41 +0200

    Background zoom and movement

--- Assets/Scripts/Core/Structure/GameManager.cs
+++ Assets/Scripts/Core/Structure/GameManager.cs
@@ -6,24 +6,18 @@ namespace Core.Structure
 {
     public class  GameManager : SingletonBase<GameManager>
     {
-        [SerializeField] private Node _nodePrefab;
-        [SerializeField] private Edge _edgePrefab;
-        
-        [SerializeField] private Transform _mapRoot;
         [SerializeField] private Transform _nodeRoot;
         [SerializeField] private Transform _edgeRoot;
+        
+        [SerializeField] private Node _nodePrefab;
+        [SerializeField] private Edge _edgePrefab;
         
         public Camera MainCamera { get; private set; }
 
         protected override void Initialize()
         {
             MainCamera = Camera.main;
-            _nodeRoot.SetParent(_mapRoot);
-            _edgeRoot.SetParent(_mapRoot);
         }
-
-        public void MoveRoot(Vector3 newPos) => _nodeRoot.position = newPos;
-        public void ScaleRoot(Vector3 newScale) => _nodeRoot.localScale = newScale;
 
         public void CreateNodeFromScreenPos(Vector2 screenPos)
         {
---
+++ Assets/Scripts/Core/Structure/PlayerController/BackgroundController.cs
@@ -0,0 +1,67 @@
+﻿using Other;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Core.Structure.PlayerController
+{
+    public class BackgroundController : MonoBehaviour, IDragHandler, IScrollHandler
+    {
+        [SerializeField] private Transform _mapRoot;
+        [SerializeField] private SpriteRenderer _spriteRenderer;
+        
+        [SerializeField] private bool _preserveBounds = true;
+        [SerializeField] private float _dragSpeed;
+        
+        [SerializeField] private float _scrollSpeed;
+        [SerializeField] private Vector2 _scrollBounds;
+        
+        private Vector2 _xBounds;
+        private Vector2 _yBounds;
+        private Vector2 _cameraHalfViewSize;
+
+        private void Awake()
+        {
+            var cam = Camera.main;
+            var height = cam!.orthographicSize;
+            var width = height * cam.aspect;
+            _cameraHalfViewSize = new Vector2(width, height);
+            
+            _spriteRenderer = GetComponent<SpriteRenderer>();
+            UpdateBounds();
+        }
+        
+        public void MoveRoot(Vector2 newPos)
+        {
+            newPos.x = _xBounds.Clamp(newPos.x);
+            newPos.y = _yBounds.Clamp(newPos.y);
+            
+            _mapRoot.position = newPos;
+        }
+
+        public void ScaleRoot(float newScale)
+        {
+            newScale = _scrollBounds.Clamp(newScale);
+            _mapRoot.localScale = new Vector3(newScale, newScale, 1f);
+        }
+
+        private void UpdateBounds()
+        {
+            var halfWidth = _spriteRenderer.size.x / 2;
+            var halfHeight = _spriteRenderer.size.y / 2;
+            
+            _xBounds = new  Vector2(-halfWidth + _cameraHalfViewSize.x, halfWidth - _cameraHalfViewSize.x);
+            _yBounds = new  Vector2(-halfHeight + _cameraHalfViewSize.y, halfHeight - _cameraHalfViewSize.y);
+        }
+        
+        public void OnDrag(PointerEventData eventData)
+        {
+            Vector3 delta = -eventData.delta;
+            MoveRoot(_mapRoot.transform.position - new Vector3(delta.x, delta.y, 0) * _dragSpeed);
+        }
+
+        public void OnScroll(PointerEventData eventData)
+        {
+            ScaleRoot(_mapRoot.localScale.x + eventData.scrollDelta.y * _scrollSpeed);
+        }
+    }
+}
