8a015d5b6bbf744c6cb386e94b25660c0405a9fc
Andrii <101969951+MechanikXD@users.noreply.github.com>
2026-02-05 14:31:00 +0200

    Fix dropdown localization issues

---
+++ Assets/Scripts/Other/LocalizeDrawKeyFromText.cs
@@ -0,0 +1,39 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.Localization.Components;
+
+namespace Other
+{
+    public class LocalizeDrawKeyFromText : MonoBehaviour
+    {
+        [SerializeField] private LocalizeStringEvent _lse;
+        [SerializeField] private TMP_Text _text;
+        private bool _wasSubscribed;
+        
+        private void Start() => SubscribeEvent();
+
+        private void OnEnable()
+        {
+            if (!_wasSubscribed) SubscribeEvent();
+        }
+
+        private void OnDisable()
+        {
+            if (_wasSubscribed) UnsubscribeEvent();
+        }
+
+        private void SubscribeEvent()
+        {
+            _wasSubscribed = true;
+            _lse.StringReference.TableEntryReference = _text.text;
+            _lse.RefreshString();
+        }
+
+        private void UnsubscribeEvent()
+        {
+            _wasSubscribed = false;
+            _lse.StringReference.TableEntryReference = default;
+            _text.SetText(string.Empty);
+        }
+    }
+}
